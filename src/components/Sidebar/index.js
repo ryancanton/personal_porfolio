ef6abe7769121806b84564fd06d9c629640a05ed
Ryan Canton <ryancanton999@gmail.com>
2023-08-15 13:50:48 -0600

    Feat: Add icon links to Github and LinkedIn accounts

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,6 +4,7 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => (
   <div className='nav-bar'>
@@ -24,6 +25,26 @@ const Sidebar = () => (
         <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
       </NavLink>
     </nav>
+    <ul>
+        <li>
+            <a 
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.linkedin.com/in/ryan-canton/"
+            >
+              <FontAwesomeIcon icon={faLinkedin} color="#4d4de" />
+            </a>
+        </li>
+        <li>
+            <a 
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/ryancanton"
+            >
+              <FontAwesomeIcon icon={faGithub} color="#4d4de" />
+            </a>
+        </li>
+    </ul>
   </div>
 )
 export default Sidebar
